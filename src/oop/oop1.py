d61ad042625befed41f3f37fc20280cc1c01175b
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-02-14 09:48:16 +0100

    solutions for inheritance concepts in OOP with tests passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Parent class
+class Vehicle():
+    pass
+
+
+# Sub class
+class FlightVehicle(Vehicle):
+    pass
+
+
+# child of the sub class
+class Starship(FlightVehicle):
+    pass
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class Airplane(Starship):
+    pass
